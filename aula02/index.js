af3e26e065bee6fad34949af6a287a0e345b45d0
Thais Bertoldo <thaisbertoldo11@gmail.com>
2023-01-24 21:40:25 -0300

    aula 2 parte 2

--- aula02/index.js
+++ aula02/index.js
@@ -30,3 +30,37 @@ titulosSelector.forEach((element) => {
   element.style.fontSize = '36px';
   element.innerText = `${element.innerText} + texto novo`;
 });
+
+// Criar elemento html
+const paragrafo = document.createElement('p');
+paragrafo.innerText = 'Meu primeiro paragrafo';
+paragrafo.style.color = 'green';
+
+const paragrafo2 = document.createElement('p');
+paragrafo2.innerText = 'Meu primeiro paragrafo2';
+paragrafo2.style.color = 'purple';
+
+// Adicionar elemento criado no html
+// document.body.appendChild(paragrafo);
+const container = document.getElementById('conteudo');
+container.appendChild(paragrafo);
+
+document.body.insertBefore(paragrafo2, container);
+
+// Remover elemento
+const elementoParaRemover = document.getElementById('titulo1');
+elementoParaRemover.remove();
+
+container.removeChild(paragrafo);
+
+// Adicionar elemento dentro de outro criado no js
+
+const teste = document.getElementById('teste');
+
+const div2 = document.createElement('div');
+const span = document.createElement('img');
+span.src = 'https://cdn-icons-png.flaticon.com/512/5231/5231019.png';
+span.setAttribute('width', 100);
+
+div2.appendChild(span);
+teste.appendChild(div2);
